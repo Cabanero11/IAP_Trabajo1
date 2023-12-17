a2890a1aaef35496f8df0996ceed35373b67fb55
Cabanero11 <cabanero1111@gmail.com>
2023-12-17 19:51:57 +0100

    transrest es el proyecto bueno de la 2a entrega
    ahhhhhhhhh

---
+++ STC_REST_API/src/main/java/error.java
@@ -0,0 +1,12 @@
+import org.mule.api.MuleEvent;
+import org.mule.api.exception.MessagingExceptionHandler;
+
+public class error implements MessagingExceptionHandler {
+
+	@Override
+	public MuleEvent handleException(Exception arg0, MuleEvent arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
